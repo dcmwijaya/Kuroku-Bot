e1d365c9bb5166b67946850192b998849c5b55b1
devancakra <mudrawijaya@gmail.com>
2023-11-06 23:10:00 +0700

    Update: index

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const token = 'API TOKEN BOT TELEGRAM ANDA';
+// Bagian inisialisasi token API Bot Telegram
+const token = '1322332711:AAEnv0oBO3ecSHQSJBmrkD2q0sKTCxTEz_0';
+
+// Bagian inisialisasi bot telegram
 const bot = new lumpia.init(token);
 
-// handle komunikasi via POST dari Telegram ke (webhook) GAS
+// Handle komunikasi via POST dari Telegram ke (webhook) GAS
 function doPost(e) {
   bot.doPost(e);
 }
 
+// Bagian ini untuk mengatur WebHook
 function setWebHook() {
   let url = 'WEB URL DEPLOY ANDA';
   let result = bot.telegram.setWebhook(url);
