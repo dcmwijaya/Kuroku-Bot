b5967cec501fab0ee1e6a119a56e87bf8523b22c
Devan C. M. Wijaya, S.Kom <54527592+devancakra@users.noreply.github.com>
2023-11-10 08:27:30 +0700

    Update startstatus.js

--- assets/command/startstatus.js
+++ assets/command/startstatus.js
@@ -25,7 +25,38 @@ bot.on('message', ctx => {
 
     // Output yang dikirim oleh bot
     ctx.replyWithHTML(msg,{ reply_markup: markup.inlineKeyboard(inline_Keyboard) }) & ctx.tg.sendMessage(admin_bot, pvmsg);
-  } else {
+  } 
+  else if (cmd === '↩️ Menu Jenis') {
+    // Data akses
+    let menu = 'jenis';
+    let user_id = ctx.from.id;
+    let name = ctx.from.first_name; if (ctx.from.last_name) { name += ' ' + ctx.from.last_name; }
+    let user = ctx.from.username; 
+
+    // Notifikasi pengakses
+    let pvmsg = '⚠️ Ada yang mengakses bot anda ❗\n\n🆔 ID : '+user_id+'\n👤 Nama : '+name;
+    pvmsg += '\n🚹 Username : @'+user+'\n📁 Akses menu : '+menu+'\n⏰ Date : '+waktu_lengkap+'.';
+    
+    // Pesan
+    let msg = '🐈 Jenis Hewan\n\nJenis hewan yang dimaksud ini berdasarkan pada kelas.\n\nSilakan pilih jenis hewan peliharaan anda dibawah ini untuk mendapatkan informasi lebih lanjut ⬇️⬇️⬇️⬇️⬇️';
+
+    // Inline keyboard
+    inline_Keyboard = [
+      [button.text('🐟 Ikan', 'sub_ikan'), button.text('🕊️ Burung', 'sub_burung')],
+      [button.text('🐇 Mamalia', 'sub_mamalia'), button.text('🐊 Reptil', 'sub_reptil')],
+      [button.text('🐸 Amfibi', 'sub_amfibi')]
+    ];
+
+    // Custom keyboard
+    custom_Keyboard = [
+      ['↩️ Menu Utama']
+    ];
+
+    // Output yang dikirim oleh bot
+    ctx.replyWithHTML(msg,{ reply_markup: markup.inlineKeyboard(inline_Keyboard) }) 
+    & ctx.tg.sendMessage(admin_bot, pvmsg,{ reply_markup: markup.keyboard(custom_Keyboard).resize() });
+    }
+  else {
     ctx.replyWithHTML('⛔ Menu yang anda pilih tidak tersedia, harap periksa kembali..');
   }
 });
