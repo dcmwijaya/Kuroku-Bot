a43e60f8091d6ed4f0fda9dead805311a584230c
Devan C. M. Wijaya, S.Kom <54527592+devancakra@users.noreply.github.com>
2023-11-10 08:18:41 +0700

    Update startstatus.js

--- assets/command/startstatus.js
+++ assets/command/startstatus.js
@@ -1,28 +1,33 @@
 // Menu Utama
-bot.hears(/start|↩️ Menu Utama/, ctx => {
-  // Data akses
-  let menu = 'start';
-  let user_id = ctx.from.id;
-  let name = ctx.from.first_name; if (ctx.from.last_name) { name += ' ' + ctx.from.last_name; }
-  let user = ctx.from.username; 
+bot.on('message', ctx => {
+  const cmd = ctx.message.text;
+  if (cmd === '/start' || cmd === '↩️ Menu Utama') {
+    // Data akses
+    let menu = 'start';
+    let user_id = ctx.from.id;
+    let name = ctx.from.first_name; if (ctx.from.last_name) { name += ' ' + ctx.from.last_name; }
+    let user = ctx.from.username; 
 
-  // Notifikasi pengakses
-  let pvmsg = '⚠️ Ada yang mengakses bot anda ❗\n\n🆔 ID : '+user_id+'\n👤 Nama : '+name;
-  pvmsg += '\n🚹 Username : @'+user+'\n📁 Akses menu : '+menu+'\n⏰ Date : '+waktu_lengkap+'.';
-   
-  // Pesan
-  let msg = '📢 Selamat datang di <b>'+username_bot+'</b>. Bot ini dapat memberikan edukasi praktis terkait bagaimana cara merawat hewan peliharaan.\n\n👋 Hai <b>'+name+'</b>...\n🚹 Username = @'+user;
-  msg += '\n\nSilakan pilih menu dibawah ini ⬇️⬇️⬇️';
+    // Notifikasi pengakses
+    let pvmsg = '⚠️ Ada yang mengakses bot anda ❗\n\n🆔 ID : '+user_id+'\n👤 Nama : '+name;
+    pvmsg += '\n🚹 Username : @'+user+'\n📁 Akses menu : '+menu+'\n⏰ Date : '+waktu_lengkap+'.';
+    
+    // Pesan
+    let msg = '📢 Selamat datang di <b>'+username_bot+'</b>. Bot ini dapat memberikan edukasi praktis terkait bagaimana cara merawat hewan peliharaan.\n\n👋 Hai <b>'+name+'</b>...\n🚹 Username = @'+user;
+    msg += '\n\nSilakan pilih menu dibawah ini ⬇️⬇️⬇️';
 
-  // Inline keyboard
-  inline_Keyboard = [
-    [button.text('🐈 Jenis', 'me_jenis'), button.text('🍗 Pakan', 'me_pakan')],
-    [button.text('🌱 Habitat', 'me_habitat'), button.text('💊 Kesehatan', 'me_kesehatan')],
-    [button.text('❓Status Bot', 'me_status')]
-  ];
+    // Inline keyboard
+    inline_Keyboard = [
+      [button.text('🐈 Jenis', 'me_jenis'), button.text('🍗 Pakan', 'me_pakan')],
+      [button.text('🌱 Habitat', 'me_habitat'), button.text('💊 Kesehatan', 'me_kesehatan')],
+      [button.text('❓Status Bot', 'me_status')]
+    ];
 
-  // Output yang dikirim oleh bot
-  ctx.replyWithHTML(msg,{ reply_markup: markup.inlineKeyboard(inline_Keyboard) }) & ctx.tg.sendMessage(admin_bot, pvmsg);
+    // Output yang dikirim oleh bot
+    ctx.replyWithHTML(msg,{ reply_markup: markup.inlineKeyboard(inline_Keyboard) }) & ctx.tg.sendMessage(admin_bot, pvmsg);
+  } else {
+    ctx.replyWithHTML('⛔ Menu yang anda pilih tidak tersedia, harap periksa kembali..');
+  }
 });
 
 
